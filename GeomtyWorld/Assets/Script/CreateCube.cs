6602543034670ea0d9c09cde95d4dcfab8bcd535
JaminsonR <jamin.r.m@hotmail.com>
2015-12-10 23:22:39 -0500

    inutil

---
+++ GeomtyWorld/Assets/Script/CreateCube.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int port1, port2, port3, port4, port5, port6;//puertos :v
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
