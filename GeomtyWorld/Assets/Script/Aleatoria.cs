6939e65ef5d22d829b44c1d8d3c06e99bf24dc0d
Xidrovo <xidrovo@hotmail.com>
2015-12-11 01:12:01 -0500

    Erroroooororororoes
    erororororororores

---
+++ GeomtyWorld/Assets/Script/Aleatoria.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aleatoria : MonoBehaviour {
+
+    private float Time;
+    private float TimeOut = 1;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Cambio", 0, TimeOut);
+	}
+
+    void Cambio()
+    {
+        TimeOut = Random.Range(5, 10);
+        transform.position = new Vector3(Random.Range(-13, 13), transform.position.y, 0);
+    }
+}
---
+++ GeomtyWorld/Assets/Script/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+
+    public float Speed = 5f;
+    public Transform Punto;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.Translate(new Vector3(Punto.transform.position.x, this.transform.position.y, 0) 
+            * Speed * Time.deltaTime);
+	}
+}
